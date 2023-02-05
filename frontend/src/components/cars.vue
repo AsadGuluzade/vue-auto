cdea2e9ff0e6e317d686d31a6db66a2c0e9775da
AsadGuluzade <guluzade.asad@gmail.com>
2023-02-05 16:18:19 +0000

    add filter

--- frontend/src/components/cars.vue
+++ frontend/src/components/cars.vue
@@ -4,7 +4,8 @@ export default {
   data() {
     return {
       cars: [],
-      selected: '',
+      selectedMake: '',
+      selectedFuel: '',
     }
   },
 
@@ -12,6 +13,19 @@ export default {
     async getData() {
       try {
         const response = await axios.get('http://localhost:4444/api/cars')
+        // JSON responses are automatically parsed.
+        this.cars = response.data
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async searchFilter(e) {
+      e.preventDefault()
+
+      try {
+        const response = await axios.get(
+          'http://localhost:4444/api/cars/filter?make=' + this.selectedMake + '&fuel=' + this.selectedFuel
+        )
         // JSON responses are automatically parsed.
         this.cars = response.data
       } catch (error) {
@@ -26,19 +40,31 @@ export default {
 </script>
 
 <template>
-  <div>Selected: {{ selected }}</div>
-
-  <select v-model="selected">
-    <option disabled value="">Please select one</option>
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
+  <form v-on:submit="searchFilter">
+    <select v-model="selectedMake">
+      <option disabled value="">Select Make</option>
+      <option>Toyota</option>
+      <option>Chevrolet</option>
+      <option>Honda</option>
+      <option>Mitsubishi</option>
+      <option>BMW</option>
+      <option></option>
+    </select>
+    <select v-model="selectedFuel">
+      <option disabled value="">Select Fuel</option>
+      <option>diesel</option>
+      <option>petrol</option>
+      <option>electric</option>
+      <option></option>
+    </select>
+    <button type="submit">Search</button>
+  </form>
   <div>
     <div v-for="car in cars" v-bind:key="car.id">
       <h2>{{ car.make }}</h2>
       <p>{{ car.model }}</p>
       <p>{{ car._id }}</p>
+      <p>{{ car.fuel }}</p>
     </div>
   </div>
 </template>
